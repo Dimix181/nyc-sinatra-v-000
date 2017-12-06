cc67bb8a7b0d2bd9a75e4645c7a30b87d9483c04
Elvin <Dimix181@Gmail.com>
2017-12-06 01:16:49 -0500

    landmark test are passing

--- app/controllers/landmarksController.rb
+++ app/controllers/landmarksController.rb
@@ -2,7 +2,38 @@ class LandmarksController < ApplicationController
 
 
   get '/' do
-      erb :'landmarks/new'
+      erb :'/landmarks/new'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks/new' do
+    @landmark = Landmark.create(params[:landmark])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+      erb :'/landmarks/index'
+  end
+
+   get "/landmarks/:id" do
+      @landmark = Landmark.lookup(params[:id])
+        erb :'/landmarks/show'
+   end
+
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.lookup(params[:id])
+      erb :'/landmarks/edit'
+  end
+
+  patch "/landmarks/:id" do
+    @landmark = Landmark.lookup(params[:id])
+     @landmark.update(params[:landmark])
+     @landmark.save
+      erb :"/landmark/#{@landmark.id}"
   end
 
 
